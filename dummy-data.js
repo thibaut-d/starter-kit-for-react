b88c1653a5aec3467c585aba4eb6cccc53765aba
Thibaut <thibaut.deveraux@gmail.com>
2020-02-07 01:13:23 +0100

    Update dummy-data.js

--- dummy-data.js
+++ dummy-data.js
@@ -1,12 +1,23 @@
 page1 = {
-   :
+   "title":"This is the page 1"
+   "creator":user1
+   "creation-date":"1995-12-17T03:24:00"
+   "modification-date":"1998-12-17T03:24:00"
+   "media":"media1.mp3"
+   "media-type":"audio"
+   "content":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+   
 }
 
 comment1 = {
-   :
+   "page":page1
+   "creator":user1
+   "creation-date":"1996-12-17T03:24:00"
+   "content":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+   
 }
 
 user1 = {
-   :
+   "name":"User 1"
 }
 
